3a0e4ab8b260a30272905a1688cb03a074ee651f
Fizban <jackmckew2@gmail.com>
2020-03-22 13:07:09 +1100

    Add test

--- tests/test_wordcount.py
+++ tests/test_wordcount.py
@@ -1,5 +1,13 @@
 from wordcount import __version__
-
+import pytest
+import textwrap
+from wordcount.markdown import count_words_in_markdown
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_markdown():
+    text = textwrap.dedent("""
+    test a b    c
+    """)
+    assert count_words_in_markdown(text) == 4
---
+++ wordcount/markdown.py
@@ -0,0 +1,33 @@
+import os
+import re
+import sys
+
+def count_words_in_markdown(markdown_text_content: str) -> int:
+
+    # Comments
+    markdown_text_content = re.sub(r'<!--(.*?)-->', '', markdown_text_content, flags=re.MULTILINE)
+    # Tabs to spaces
+    markdown_text_content = markdown_text_content.replace('\t', '    ')
+    # More than 1 space to 4 spaces
+    markdown_text_content = re.sub(r'[ ]{2,}', '    ', markdown_text_content)
+    # Footnotes
+    markdown_text_content = re.sub(r'^\[[^]]*\][^(].*', '', markdown_text_content, flags=re.MULTILINE)
+    # Indented blocks of code
+    markdown_text_content = re.sub(r'^( {4,}[^-*]).*', '', markdown_text_content, flags=re.MULTILINE)
+    # Custom header IDs
+    markdown_text_content = re.sub(r'{#.*}', '', markdown_text_content)
+    # Replace newlines with spaces for uniform handling
+    markdown_text_content = markdown_text_content.replace('\n', ' ')
+    # Remove images
+    markdown_text_content = re.sub(r'!\[[^\]]*\]\([^)]*\)', '', markdown_text_content)
+    # Remove HTML tags
+    markdown_text_content = re.sub(r'</?[^>]*>', '', markdown_text_content)
+    # Remove special characters
+    markdown_text_content = re.sub(r'[#*`~\-–^=<>+|/:]', '', markdown_text_content)
+    # Remove footnote references
+    markdown_text_content = re.sub(r'\[[0-9]*\]', '', markdown_text_content)
+    # Remove enumerations
+    markdown_text_content = re.sub(r'[0-9#]*\.', '', markdown_text_content)
+
+    return len(markdown_text_content.split())
+
