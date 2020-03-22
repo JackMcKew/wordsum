c015faab74478442ffe3538de456ea07b63461c6
Fizban <jackmckew2@gmail.com>
2020-03-22 13:27:13 +1100

    Add checkpoint exception

--- wordcount/util/file_locate.py
+++ wordcount/util/file_locate.py
@@ -1,13 +1,18 @@
 import pathlib
 from typing import List
 def locate_files(path: str,file_type:str) -> List[pathlib.Path]:
-    topPath: pathlib.Path = pathlib.Path(path)
+    top_path: pathlib.Path = pathlib.Path(path)
 
     if "." not in file_type:
         print("File types must include a period eg, '.md'")
 
-    for singleFile in topPath.glob(f'**/*{file_type}'):
-        if singleFile.suffix == '.md':        
-            allMarkdown.append(singleFile)
-        if singleFile.suffix == '.ipynb' and 'checkpoint' not in singleFile.name:
-            allJupyter.append(singleFile)
+    located_files: List[pathlib.Path] = []
+
+    for found_file_path in top_path.glob(f'**/*{file_type}'):
+        if file_type != '.ipynb':
+            located_files.append(found_file_path)
+        else:
+            if 'checkpoint' not in found_file_path.name:
+                located_files.append(found_file_path)
+
+    return located_files
