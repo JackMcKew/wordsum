1001e7c27e1837271f33fc3562eb1574f9dca002
Fizban <jackmckew2@gmail.com>
2020-03-25 14:58:41 +1100

    Remove flake8

--- tests/test_wordsum.py
+++ tests/test_wordsum.py
@@ -7,33 +7,39 @@ from wordsum._util.file_locate import locate_files
 from wordsum._io.read_files import read_path_list
 import wordsum
 
+
 def test_version():
-    assert __version__ == '0.1.3'
+    assert __version__ == "0.1.3"
+
 
 def test_markdown():
-    text = textwrap.dedent("""
+    text = textwrap.dedent(
+        """
     test a b    c
-    """)
+    """
+    )
     assert count_words_in_markdown(text) == 4
 
+
 def test_locate_files():
-    file_list = locate_files('./example_files','.md')
+    file_list = locate_files("./example_files", ".md")
     assert len(file_list) == 1
 
-@pytest.mark.parametrize('file_type, expected', [
-    ('.md',1),
-    ('.ipynb',1),
-])
-def test_read_files(file_type,expected):
-    file_list = locate_files('./example_files','.md')
+
+@pytest.mark.parametrize("file_type, expected", [(".md", 1), (".ipynb", 1),])
+def test_read_files(file_type, expected):
+    file_list = locate_files("./example_files", ".md")
     file_content = read_path_list(file_list)
     assert len(file_content) == expected
 
+
 def test_count_markdown():
-    assert wordsum.count_words('./example_files',['.md']) == 4
+    assert wordsum.count_words("./example_files", [".md"]) == 4
+
 
 def test_count_jupyter():
-    assert wordsum.count_words('./example_files',['.ipynb']) == 4
+    assert wordsum.count_words("./example_files", [".ipynb"]) == 4
+
 
 def test_count_all():
-    assert wordsum.count_words('./example_files',wordsum.list_supported_formats()) == 8
+    assert wordsum.count_words("./example_files", wordsum.list_supported_formats()) == 8
